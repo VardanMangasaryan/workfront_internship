ecf685bc77667d2da2aaaa66d15f6ca1f77c8fdb
Vardan <vardanmangasaryan@workfront.com>
2020-04-11 14:06:49 +0400

    Second exercise

--- secondHomework.js
+++ secondHomework.js
@@ -1,7 +1,7 @@
 // 1. Insert a number. Print ‘yes’ if the number is prime, ‘no’ otherwise.
 
 function isNumPrime(num) {
-    let isPrime = true;
+    let isPrime = "yes";
     let i = 2;
     num = Math.abs(num);
     if (num < 3) {
@@ -9,14 +9,23 @@ function isNumPrime(num) {
     } else {
         while (i <= Math.round(num / 2)) {
             if (num % i === 0) {
-                isPrime = false;
+                isPrime = "no";
                 break
             } else {
                 i++
             }
         }
     }
-    return `Is prime = ${isPrime}`
+    return isPrime
 }
 
-console.log(isNumPrime(3));
+
+//2. Given a number n ( n>= 0 ). Print nth Fibonacci number.  (Fibonacci series: 0, 1, 1, 2, 3, 5, 8 …, ak = ak-1 + ak-2)
+
+function sequenceFibonacci(n) {
+    if (n === 0 || n === 1) {
+        return n
+    } else {
+        return sequenceFibonacci(n - 1) + sequenceFibonacci(n - 2)
+    }
+}
