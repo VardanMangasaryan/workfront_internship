a46f54722ab86249f4ba85caf7797ad37392c3b7
Vardan <vardanmangasaryan@workfront.com>
2020-04-11 16:34:30 +0400

    Exercise 5

--- secondHomework.js
+++ secondHomework.js
@@ -20,7 +20,6 @@ function isNumPrime(num) {
 }
 
 
-
 //2. Given a number n ( n>= 0 ). Print nth Fibonacci number.  (Fibonacci series: 0, 1, 1, 2, 3, 5, 8 …, ak = ak-1 + ak-2)
 
 function numberFibonacci(n) {
@@ -32,20 +31,64 @@ function numberFibonacci(n) {
 }
 
 
-
 //3. Given a number n( n> 0 ). Print Fibonacci series up to n
 function sequenceFibonacci(n) {
     let last = 1;
     let numFib = 0;
     let secondLast = 0;
     let completeSequence = "";
-    let i = 0;
-    while(numFib + secondLast < n){
+    while (numFib + secondLast < n) {
         numFib += secondLast;
         secondLast = last;
         last = numFib;
         completeSequence += `${numFib} `;
-        i++
     }
     return completeSequence
+}
+
+//4. Insert a number. Calculate product and sum of the digits of the number. If product is divisible by the sum, print the quotient, otherwise print the remainder.
+
+
+function fourthExercise(num) {
+    if (num === 0) {
+        return "Cannot calculate"
+    }
+    num = num;
+    let sum = 0;
+    let prod = 1;
+    let div;
+    for (let i = 0; i < num.toString().length; i++) {
+        sum += num[i];
+        prod *= num[i];
+    }
+    div = prod % sum;
+    if (div === 0) {
+        return `Quotient is ${prod / sum}`
+    } else {
+        return `Reminder is ${div}`
+    }
+}
+
+
+//5. Given three numbers a, b (a ≤ b) and num. Create an array of evenly spaced numbers by the given num length over the specified interval (from a to b).
+
+function returnNumbers(a, b, num) {
+    let listOfNumbers = [];
+    if (num === 1) {
+        listOfNumbers.push(a);
+        return listOfNumbers
+    } else if (num === 2) {
+        listOfNumbers.push(a, b);
+        return listOfNumbers
+
+    } else {
+        listOfNumbers.push(a);
+        let d = (b - a) / (num-1);
+        for (let i = 0; i < num - 2; i++) {
+            a += d;
+            listOfNumbers.push(a)
+        }
+        listOfNumbers.push(b)
+    }
+    return listOfNumbers
 }
