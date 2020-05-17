1d97f96d27a070a3f479d0ceb3cc0b84e7d2c574
Vardan <vardanmangasaryan@workfront.com>
2020-05-17 23:01:38 +0400

    4

--- sixthHomework.js
+++ sixthHomework.js
@@ -4,8 +4,24 @@ function isAllUnique(arr) {
     return arr.length === arrToSet.size;
 }
 
+
 // 3. Check whether string is palindrome, or not.
 function isPalindrome(str) {
     let reversedStr = str.split("").reverse().join("");
     return str === reversedStr;
+}
+
+
+// 4.Given a word and a list of possible anagrams, select the correct sublist.
+function isThereAnagrams(word, arr) {
+    let sortedWord = JSON.stringify(word.split("").sort());
+    let splitWords = arr.map(el => el.split("").sort());
+    let anagramWords = [];
+    splitWords.forEach((el, index) => {
+            if (JSON.stringify(el) === sortedWord) {
+                anagramWords.push(arr[index])
+            }
+        }
+    );
+    return anagramWords
 }
