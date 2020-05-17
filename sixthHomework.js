a9a63ecf8cfb02d97d9d932b2b7f7b390082ae6f
Vardan <vardanmangasaryan@workfront.com>
2020-05-17 18:45:27 +0400

    3

--- sixthHomework.js
+++ sixthHomework.js
@@ -4,3 +4,8 @@ function isAllUnique(arr) {
     return arr.length === arrToSet.size;
 }
 
+// 3. Check whether string is palindrome, or not.
+function isPalindrome(str) {
+    let reversedStr = str.split("").reverse().join("");
+    return str === reversedStr;
+}
